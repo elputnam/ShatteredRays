3f41b16f1c3f14da80e8ecb473b59867d562d4cf
fleshcircuit <elputnam@inaction.ie>
2022-05-02 14:49:45 +0100

    many changes

--- js/sketch.js
+++ js/sketch.js
@@ -11,10 +11,19 @@ let particle;
 let xoff = 0;
 let yoff = 10000;
 
+//ccapture
+// const T = 1;
+// const NUM_FRAMES = 200;
+// var capture = false; // default is to not capture frames, can be changed with button in browser
+var capturer = new CCapture({
+  format:'webm', 
+  framerate: 60
+});
+
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(1920, 1080);
   colorMode(HSB, 360, 100, 100, 100);
-  for (let i = 0; i < 5; i++) {
+  for (let i = 0; i < 10; i++) {
     let x1 = random(width);
     let x2 = random(width);
     let y1 = random(height);
@@ -29,7 +38,8 @@ function setup() {
 }
 
 function draw() {
-  background(0);
+  if (frameCount==1) capturer.start(); // start the animation capture
+  background(255);
   for (let wall of walls) {
     wall.show();
   }
@@ -39,4 +49,13 @@ function draw() {
 
   xoff += 0.01;
   yoff += 0.01;
+  
+capturer.capture(document.getElementById('defaultCanvas0')); 
+  if (frameCount==1200){
+    save_record();
+  }
+}
+
+function save_record() {
+  capturer.save();
 }
